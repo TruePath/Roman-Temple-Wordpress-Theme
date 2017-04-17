926d1dfe4e123bebcca05359882d0154a6185526
Peter Gerdes <gerdes@invariant.org>
2017-04-17 21:37:28 +0300

    fixed enques to include style

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-function b4st_enqueues() {
+function roman_temple_enqueues() {
 
 	/* Styles */
 
@@ -11,8 +11,7 @@ function b4st_enqueues() {
 	wp_register_style('font-awesome-css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', false, '4.7.0', null);
 	wp_enqueue_style('font-awesome-css');
 
- //  	wp_register_style('b4st-css', get_template_directory_uri() . '/theme/css/b4st.css', false, null);
-	// wp_enqueue_style('b4st-css');
+  	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 	/* Scripts */
 	wp_deregister_script('jquery');
@@ -39,10 +38,10 @@ function b4st_enqueues() {
 	wp_enqueue_script('bootstrap-js');
 
 	wp_register_script('b4st-js', get_template_directory_uri() . '/javascripts/b4st.js', false, null, true);
-	wp_enqueue_script('b4st-js');
+	// wp_enqueue_script('b4st-js');
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
 }
-add_action('wp_enqueue_scripts', 'b4st_enqueues', 100);
+add_action('wp_enqueue_scripts', 'roman_temple_enqueues', 100);
