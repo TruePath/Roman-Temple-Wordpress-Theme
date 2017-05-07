711d2963215b96e7807cb41362cebda7cfe5e006
Peter Gerdes <gerdes@invariant.org>
2017-05-07 19:54:38 +0300

    widening left sidebar

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <div class="container-fluid">
   <div class="row">
 
-    <div class="col-lg-2" id="left-sidebar" role="navigation">
+    <div class="col-lg-3" id="left-sidebar" role="navigation">
        <?php get_sidebar('left'); ?>
     </div>
     
-      <div id="content" class="col-lg-7" role="main">
+      <div id="content" class="col-lg-6" role="main">
           <?php get_template_part('loops/content', get_post_format()); ?>
       </div><!-- /#content -->
     
