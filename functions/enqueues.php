c35286b07e657fd1fea591f5453b5a85f885dc04
Peter Gerdes <gerdes@invariant.org>
2017-05-14 10:43:37 +0300

    moved javascripts out of head and a few style upgrades

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -8,7 +8,7 @@ function b4sass_enqueues() {
 
 	 wp_deregister_script('jquery-migrate');
     // Register
-    wp_register_script('jquery-migrate', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.min.js', array('jquery'), '3.0.0'); // require jquery, as loaded above
+    wp_register_script('jquery-migrate', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.min.js', array('jquery'), '3.0.0', true); // require jquery, as loaded above
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'jquery-migrate' );
 	// wp_deregister_script('jquery-ui');
@@ -19,15 +19,15 @@ function b4sass_enqueues() {
  //  	wp_register_script('modernizr',  'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', false, '2.8.3', true);
 	// wp_enqueue_script('modernizr');
 
-	wp_register_script('tether',  'https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', false, '1.4.0', false);
+	wp_register_script('tether',  'https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', false, '1.4.0', true);
 	wp_enqueue_script('tether');
 
 	// wp_register_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js', array('jquery'), '4.0.0-alpha.6', false);
-  	wp_register_script('bootstrap-js', get_template_directory_uri() . '/javascripts/bootstrap.min.js', array('jquery'), '4.0.0-alpha.6', false);
+  	wp_register_script('bootstrap-js', get_template_directory_uri() . '/javascripts/bootstrap.min.js', array('jquery'), '4.0.0-alpha.6', true);
 	wp_enqueue_script('bootstrap-js');
 
 	wp_register_script('b4st-js', get_template_directory_uri() . '/javascripts/b4st.js', false, null, true);
-	// wp_enqueue_script('b4st-js');
+	wp_enqueue_script('b4st-js');
 
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
